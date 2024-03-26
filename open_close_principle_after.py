20d0e6c70c11ca7861b12b60614aa6a79ff70d5c
syed <you@example.com>
2024-03-27 03:05:04 +0530

    open close principle complete

---
+++ open_close_principle_after.py
@@ -0,0 +1,61 @@
+# Open close principle stands for open for extension and close for modification
+# In payment pocess We can't add new payment pethod without having to change existing code.
+# Resulting violates open close principle
+
+# Now IT dont' violates Open close principle.
+# If we want to any new payment method such as Paypal, UPI , We have to just create new class.
+# Start using it.
+
+from abc import ABC, abstractmethod
+
+class Order:
+    
+    def __init__(self):
+        self.items: list[str]=[]
+        self.quantities: list[str]=[]
+        self.prices: list[float]=[]
+        self.payment_status: bool = False
+        
+    def add_item(self, item, quantity, price):
+        self.items.append(item)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+        
+    def total_price(self)-> float:
+        
+        total: float = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i]*self.prices[i]
+            
+        return total
+ 
+class PaymentProcess(ABC):
+    
+    @abstractmethod
+    def pay(self, payment_type, security_code):
+        pass
+    
+               
+class DebitPaymentProcess(PaymentProcess):
+    
+    def pay(self, order, security_code):
+        print(f"Initializig Debit Payment type: ")
+        print(f"Verifying Secirity Code: {security_code}")
+        order.payment_status = True
+        print("Payment Success")
+        
+class CreditPaymentProcess(PaymentProcess):
+    
+    def pay(self, order, security_code):
+        print(f"Initializig Credit Payment type: ")
+        print(f"Verifying Secirity Code: {security_code}")
+        order.payment_status = True
+        print("Payment Success")
+
+        
+order = Order()
+order.add_item('Laptop', 2, 45000)
+order.add_item('HDD', 1, 2000)
+print(f"Total price: {order.total_price()}")
+payment=DebitPaymentProcess()
+payment.pay(order, '90r3434')
---
+++ open_close_principle_before.py
@@ -0,0 +1,49 @@
+# Open close principle stands for open for extension and close for modification
+# In payment pocess We can't add new payment pethod without having to change existing code.
+# Resulting violates open close principle
+
+class Order:
+    
+    def __init__(self):
+        self.items: list[str]=[]
+        self.quantities: list[str]=[]
+        self.prices: list[float]=[]
+        self.payment_status: bool = False
+        
+    def add_item(self, item, quantity, price):
+        self.items.append(item)
+        self.quantities.append(quantity)
+        self.prices.append(price)
+        
+    def total_price(self)-> float:
+        
+        total: float = 0
+        for i in range(len(self.prices)):
+            total += self.quantities[i]*self.prices[i]
+            
+        return total
+
+class PaymentProcess:
+    
+    def payment_process(self, order, payment_type, security_code):
+        
+        if payment_type == 'debit':
+            print(f"Initializig Debit Payment type: ")
+            print(f"Verifying Secirity Code: {security_code}")
+            order.payment_status = True
+            print("Payment Success")
+        elif payment_type == 'credi':
+            print(f"Initializig Credi Payment type: ")
+            print(f"Verifying Secirity Code: {security_code}")
+            order.payment_status = True
+            print("Payment Success")
+        
+        else:
+            print("Invalid Payment Method")
+        
+order = Order()
+order.add_item('Laptop', 2, 45000)
+order.add_item('HDD', 1, 2000)
+print(f"Total price: {order.total_price()}")
+payment=PaymentProcess()
+payment.payment_process(order,'debit', '90r3434')
